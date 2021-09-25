8eb9395f0bef193d64b87fb5ae7106a9ea48207a
Kirill <90552397+XoDefender@users.noreply.github.com>
2021-09-25 10:59:09 +0300

    Add files via upload

---
+++ lab_4/lab_4/Source.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	// task 1
+	/*int a = 5;
+	int b = 6;
+	
+	int s = a * b;
+	int p = 2 * (a + b);
+
+	cout << "S = " << s << endl;
+	cout << "P = " << p << endl;*/
+
+	// task 2
+	/*int d = 5;
+	int l = 3.14 * d;
+
+	cout << l << endl;*/
+
+	// task 3
+	/*float a = 5.0;
+	float b = 6.0;
+
+	float median = (a + b) / 2;
+	cout << median << endl;*/
+
+	// task 4
+	/*int a = 5;
+	int b = 6;
+
+	int mult = (a * a) * (b * b);
+	int sum = a + b;
+	int differ = a - b;
+	int division = (a * a) / (b * b);
+
+	cout << mult << " " << sum << " " << differ << " " << division << endl;*/
+
+	// task 5
+	/*int a = 5;
+	int b = 6;
+
+	int mult = abs(a) * abs(b);
+	int sum = abs(a) + abs(b);
+	int differ = abs(a) - abs(b);
+	int division = abs(a) / abs(b);*/
+}
