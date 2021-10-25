f7ffecb788b511fefc8fbd5e7f0bb4ffca6d07f8
Kirill <90552397+XoDefender@users.noreply.github.com>
2021-10-25 09:35:01 +0300

    Add files via upload

---
+++ lab_9/lab_9/Source.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+    // task 1
+    /*int a;
+    cout << "Enter the amount of seconds: ";
+    cin >> a;
+    while (a - 60 >= 0)
+    {
+        a-= 60;
+    }
+    cout << "amount of seconds from the last minute = " << a << endl;*/
+
+    // task 2
+    /*int a;
+    cout << "enter the day of a year: ";
+    cin >> a;
+    cout << "the number of the " << a << "day of the year: " << a % 7 << endl;*/
+
+    // task 3
+   /* int a, n;
+    cout << "enter the day of a year: ";
+    cin >> a;
+    cout << "enter the day of a 1 january: ";
+    cin >> n;
+    cout << "the number of the week day " << a << " of the year dat:" << ((a + n - 2) % 7) + 1 << endl;*/
+
+    //task 4
+    /*int a, b, c,length,height;
+    cout << "the A value: ";
+    cin >> a;
+    cout << "the B value: ";
+    cin >> b;
+    cout << "the C value: ";
+    cin >> c;
+
+    length = b / c;
+    height = a / c;
+
+    int sq = length * height;
+
+    cout << "the amount of squares in a rectangle = " << sq << endl;
+    cout << "the free area of the rectangle = " << (a * b) - (c * c * sq) << endl;*/
+
+    // task 5
+    //int a;
+    //cout << "the year number: ";
+    //cin >> a;
+    //cout << "the century number = " << (a - 1) / 100 + 1 << endl;
+}
