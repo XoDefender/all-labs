701868925222500ed65f8d485e65d1e37077ebdf
Kirill <90552397+XoDefender@users.noreply.github.com>
2021-10-25 09:35:19 +0300

    Add files via upload

---
+++ lab_10/lab_10/Source.cpp
@@ -0,0 +1,116 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+void main() {
+    // task 1
+    /*int a, b;
+    bool check = true;
+
+    cout<<"a > 2 и b ≤ 3"<<endl;
+    cout << "enter the a: ";
+    cin >> a
+    cout << "enter the b: ";
+    cin >> b;
+
+    if (a > 2 && b <= 3)
+        cout << check << endl;
+    else {
+        check = false;
+        cout << check;
+    }*/
+
+    // task 2
+    /*int a, b, c;
+    bool check = true;
+
+    cout << "a < b < c" << endl;
+    cout << "enter the a: ";
+    cin >> a;
+    cout << "enter the b: ";
+    cin >> b;
+    cout << "enter the c: ";
+    cin >> c;
+
+    if (a < b && b < c)
+        cout << check << endl;
+    else {
+        check = false;
+        cout << check;
+    }*/
+
+    // task 3
+    /*int a;
+    bool check = true;
+
+    cout << "is the number two-digit?" << endl;
+    cout << "enter the number: ";
+    cin >> a;
+
+    if (a > 9 && a < 100 && a % 2 == 0)
+        cout << check << endl;
+    else {
+        check = false;
+        cout << check;
+    }*/
+
+    // task 4
+    /*int a;
+    bool check = true;
+
+    cout << "enter the three-digit number: ";
+    cin >> a;
+    if ((a / 100 < (a % 100 / 10) && (a % 100 / 10) < a % 10) || (a / 100 > (a % 100 / 10) && (a % 100 / 10) > a % 10))
+        cout << check << endl;
+    else {
+        check = false;
+        cout << check;
+    }*/
+
+    // task 5
+    /*int a;
+    bool check = true;
+
+    cout << "is the number palindrome?" << endl;
+    cout << "enter the four-digit number: ";
+    cin >> a;
+
+    if (a % 10 == a / 1000 && a % 100 / 10 == a % 1000 / 100)
+        cout << check << endl;
+    else {
+        check = false;
+        cout << check;
+    }*/
+
+    // task 6
+    /*int a, b, c;
+    cout << "right triangle" << endl;
+    cout << "enter the a: ";
+    cin >> a;
+    cout << "enter the b: ";
+    cin >> b;
+    cout << "enter the c: ";
+    cin >> c;
+
+    if ( (a * a == b * b + c * c) || (b * b == a * a + c * c) || (c * c == a * a + b * b) )
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;*/
+
+    // task 7
+    /*int a, b, c;
+
+    cout << "is it a triangle?" << endl;
+    cout << "enter the A: ";
+    cin >> a;
+    cout << "enter the B: ";
+    cin >> b;
+    cout << "enter the C: ";
+    cin >> c;
+
+    if ( (a + b) > c && (a + c) > b && (b + c) > a )
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;*/
+}
